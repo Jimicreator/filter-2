2375645865e608049c6bfaa10f61494dd0aca04d
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-05-23 23:14:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,21 +1,61 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {} 😁😁
+    START_MSG = """ <b>Hi {} ☺️☺️
 ഞാൻ <a href:"@jointhisdiscussion">Malayalam Rockers</a> ഗ്രൂപ്പിൽ ഉപയോഗിക്കാൻ വേണ്ടി നിർമ്മിച്ച ബോട്ട് ആണ്.
 വേറെ ആർക്കും പറ്റില്ല.🤣🤣
 
+"""
 
-    HELP_MSG = """ ഇവിടെ ഒന്നുമില്ല. """
+
+    HELP_MSG = """<b>How to use the bot??</b>
+
+<i>
+* Add bot to your group with admin rights.
+
+* Add bot to channels which you want to link with <b>all admin rights</b>!
+</i>
 
 
-    ABOUT_MSG = """
-⭕️<b>My Name : Thomas Shelby</b>
+<b>Bot Commands - Works in Group only</b>
+(You need to be a Auth User in order to use these commands)
+
+* <code>/add channelid</code>  -  Links channel to your group.
+or
+* <code>/add @channelusername</code> - Links channel to your group.
+
+<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+
+
+* <code>/del channelid</code>  -  Delinks channel from group
+or
+* <code>/del @channelusername</code>  -  Delinks channel from group
+
+<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+
+
+* <code>/delall</code>  -  Removes all connected channels and filters from group!
+
+<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
+
+
+* <code>/filterstats</code>  -  Check connected channels and number of filters.
+
+
+
+No need add each filter again!
+Bot will automatically search for your files and give links to that!
+
+
+<b>© @TroJanzHEX</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
 ⭕️<b>Creater :</b> @null_fallaci
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>  
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 """
